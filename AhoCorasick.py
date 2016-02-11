93f5707daf9ea3a46a8532a4017efde009b58a7f
Chengifei <yifeizheng43@gmail.com>
2016-02-11 11:51:24 +0800

    init commit in Aho-Corasick

--- AhoCorasick.py
+++ AhoCorasick.py
@@ -3,60 +3,71 @@ Created on Feb 4, 2016
 
 @author: YFZHENG
 '''
-import collections
-dict = collections.OrderedDict()
+from pdb import set_trace
+# import collections
+# dict = collections.OrderedDict()
+
 
 class ACtrie():
 
     def __init__(self, patternset):
         self.set = patternset
-        T = map(len, patternset)
-        self.maxlen = max(T)
+        _len = map(len, patternset)
+        self.maxlen = max(_len)
         self.root = ACnode('ROOT', None, False)
         # should probably move to be class variable
-        self.allnodes = {}  # should probably be a set
-        self.nodesbylevel = dict.fromkeys(range(self.maxlen + 1), set())
+        self.allnodes = set()
+        self.nodesbylevel = [set() for i in range(self.maxlen + 1)]
         # self.root not included
+        self.curnode = self.root
         for i in self.set:
             self.add(i)
 
     def add(self, str):
         parent = self.root
         for i in str[:-1]:
-            if (i, parent) not in self.allnodes:
-                self.allnodes[(i, parent)] = ACnode(i, parent, str)
-                self.nodesbylevel[parent.level + 1].add(self.allnodes[(i, parent)])
-                parent = self.allnodes[(i, parent)]
-        ACnode(str[-1], parent, corrStr=str)
+            # set_trace()
+            _T = ACnode(i, parent)
+            if _T not in self.allnodes:
+                self.allnodes.add(_T)
+                self.nodesbylevel[parent.level + 1].add(_T)
+                parent = _T
+        self.allnodes.add(ACnode(str[-1], parent, str))  # mark end of string
         # Heretofore, all parent-child relation linked
         self.link()  # link all redirection in AC-trie
 
     def link(self):  # linker
-        for i in reversed(self.nodesbylevel):  # mark redirection from bottom
+        for node in self.nodesbylevel[1]:
+            node.link(self.root)
+        for i in range(self.maxlen, 1, -1):  # link redirection from bottom
             for node in self.nodesbylevel[i]:
-                for j in self.nodesbylevel:  # from the 1st level (not zeroth)
+                for j in range(1, i):  # from the 1st level (not zeroth)
                     for Node in self.nodesbylevel[j]:
                         if node.char == Node.char:
-                            node.link(j)
-                        break
-                if not node.suffixset:
+                            node.link(Node)
+                            break
+                    break
+                if not node.redirect:
+                    print(node, 'linked to root')
                     node.link(self.root)
-    
-    def go(self, str):
+
+    def proc(self, str):
         level = 0  # at root
         for i in str:
             if i in self.nodesbylevel[level + 1]:
                 level += 1
         pass
 
+
 class ACnode():
 
-    def __init__(self, char, parent, corrStr=None):
+    def __init__(self, char, parent, endOfStr=False):
         self.char = char
         self.parent = parent
-        self.corrStr = corrStr  # corresponding string
-        self.childset = set()
-        self.suffixset = set()
+        self.isEndOfStr = endOfStr  # corresponding string
+        self.children = set()
+        self.redirect = None
+        self.repr = str(self.char) + str(self.parent)
         try:
             self.level = parent.level + 1
         except AttributeError:
@@ -64,18 +75,25 @@ class ACnode():
         if parent:
             parent.link(self)
 
-    def link(self, other, type=0):  # 0 for child, 1 for suffix
-        if type == 0:
-            self.childset.add(other)
-        elif type == 1:
-            self.suffixset.add(other)
+    def link(self, other, type=1):  # 0 for child, 1 for redirection
+        if type:
+            self.redirect = other
+        else:
+            self.children.add(other)
+
+    def __repr__(self):
+        if self.char == 'ROOT':
+            return ''
+        return self.repr
 
     def __eq__(self, other):
-        return self.char == other.char and self.parent == other.parent
+        print(hash(self) - hash(other))
+        return self.repr == other.repr
 
     def __hash__(self):
-        return hash((self.char, self.parent))
+        return hash(self.repr)
 
 if __name__ == '__main__':
-    struct = ACtrie({'a', 'ab', 'bab', 'bc', 'bca', 'c', 'caa'})
-    print(struct.nodesbylevel)
+    # struct = ACtrie({'a', 'ab', 'bab', 'bc', 'bca', 'c', 'caa'})
+    struct = ACtrie({'a', 'ab', 'bab', 'bc'})
+    print(struct.allnodes)
