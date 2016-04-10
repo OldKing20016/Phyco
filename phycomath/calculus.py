be9ff6164da07fcac9805438f89d265129f5d213
Chengifei <yifeizheng43@gmail.com>
2016-04-10 12:29:54 +0800

    Added trig func support in differentiation
    not tested yet

--- phycomath/calculus.py
+++ phycomath/calculus.py
@@ -28,32 +28,54 @@ def diff(expr: plexpr, var: str):
                              plexpr('*', diff(expr[2], var), expr[1])),
                       plexpr('*', expr[2], expr[2]))
 
+    # TODO: differentiation of power may fail when in some special conditions
     def dpow(expr, var):
         return plexpr('*',
                       copy(expr),
                       plexpr('+',
                              plexpr('*',
                                     diff(expr[2], var),
-                                    plexpr('ln(', None, expr[1])),
+                                    plexpr('ln(', None, expr[1])
+                                    ),
                              plexpr('/',
                                     plexpr('*',
                                            expr[2],
-                                           diff(expr[1], var)),
+                                           diff(expr[1], var)
+                                           ),
                                     expr[1]
                                     )
-                             ))
+                             )
+                      )
 
     def dln(expr, var):
         return plexpr('/', diff(expr[2], var), copy(expr))
 
+    def dsin(expr, var):
+        return plexpr('*', plexpr('cos(', None, expr[2]), diff(expr[2], var))
+
+    def dcos(expr, var):
+        return plexpr('*',
+                      plexpr('-', 0, plexpr('sin(', None, expr[2])),
+                      diff(expr[2], var))
+
+    def dtan(expr, var):
+        return plexpr('*',
+                      plexpr('+',
+                             1,
+                             plexpr('^', plexpr('tan', None, copy(expr[2])), 2)
+                             ),
+                      diff(expr[2], var)
+                      )
+
     corrRule = {'+': dadd, '-': dsub, '*': dmul, '/': ddiv, '^': dpow,
-                'ln(': dln}
+                'sin': dsin, 'cos': dcos, 'tan': dtan, 'ln': dln}
 
     if type(expr) is plexpr:
         try:
             return corrRule[expr.stroperator](expr, var)
         except KeyError:
-            raise UnderConstruction
+            raise UnderConstruction('The differentiation rule of the'
+                                    'requested function is not supported now.')
     elif expr is var:
         return 1
     else:
