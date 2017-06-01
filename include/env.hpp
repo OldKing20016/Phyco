61f5e7052bb0fb70904f8d4e4d6966fc0cdd04d0
Chengifei <yifeizheng43@gmail.com>
2017-06-01 10:10:32 +0800

    env header now define macros to values

--- include/env.hpp
+++ include/env.hpp
@@ -4,6 +4,11 @@
  *
  * This header governs all environment or platform dependent macros.
  * All platform-dependent header shall include this file directly or indirectly.
+ *
+ * This header should contain only compile time routines, e.g. constexpr
+ * variables, functions, as well as plain old macros. All symbols defined in
+ * this header shall starts with 'ATOM_'.
+ * Please avoid bare macro definition, and always use #define ABC (value).
  */
 
 // Detect AVX support
@@ -15,20 +20,20 @@
 
 // Check if we need to zeroupper
 #ifdef ATOM_AVX
-#define ATOM_NEED_ZEROUPPER
+#define ATOM_NEED_ZEROUPPER 1
 #endif
 
 // Detect FMA support
 #ifdef __FMA__
-#define ATOM_FMA
+#define ATOM_FMA 1
 #endif
 
 // Use reciprocals as a optimization for divisions?
-#define ATOM_RCP_AS_DIV
+#define ATOM_RCP_AS_DIV 1
 
 // Check if we're compiling with Visual C++ (CL)
 #ifdef _MSC_VER
-#define ATOM_MSVC
+#define ATOM_MSVC 1
 #endif
 
 // How many threads are available or preferable? (This is currently unimplemented)
